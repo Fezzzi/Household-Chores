c3b4395d35e8f1ee5dfd6edaee6e2ac1573dd670
Filip Horky <Fezzzi@users.noreply.github.com>
2020-04-06 21:46:13 +0200

    #2 - Server bootstrap

---
+++ code/server/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const app = require('./app')
+const database = require('./database')
+const chalk = require('chalk')
+
+// Declare default port
+const PORT = process.env.PORT || 9000;
+
+// Connect to the database
+/*database.connect(error => {
+
+  // Upon failure...
+  if (error) {
+    console.log(chalk.red('Could not connect to the database.'))
+    console.error(error)
+    return
+  }
+
+  // Upon success...
+  console.log('Established database connection.')
+*/
+  // Start the server
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}!`)
+  })/*
+})
+*/
