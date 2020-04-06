ad520ff5100c55a3fb4e623128d6c48fc99c66a6
Filip Horky <Fezzzi@users.noreply.github.com>
2020-04-06 21:53:43 +0200

    #1 - Add configuration files
    Added mostly random configuration for:
     - npm
     - typescript
     - webpack
     - babel
     - eslint

---
+++ config/webpackResolver.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = ({
+    context: path.resolve(__dirname, '../code/client'),
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+    },
+});
