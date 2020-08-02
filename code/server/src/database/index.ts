a42ac180f0b28c49831af80302b81c22cdbe5e1b
Fezzzi <filip.horky.cz@gmail.com>
2020-08-02 17:52:41 +0200

    #25 - Use safer type of db queries

--- code/server/src/database/index.ts
+++ code/server/src/database/index.ts
@@ -3,18 +3,22 @@ import util from 'util';
 import { Connection, handleConnectionError } from './connection';
 import { Logger } from '../helpers/logger';
 import { DB_LOG, ERROR_LOG } from '../constants/logs';
+import {queryCallback} from "~/node_modules/@types/mysql";
 
 const toLine = (sql: string) => sql.replace('\n', '');
 
+const getQueryPromise = (): ((options: string, values: any, callback?: queryCallback) => Promise<any>) =>
+  util.promisify(Connection.get().query);
+
 export const database = {
-  query: (sql: string, logSQL = true): any =>
-    util.promisify(Connection.get().query)
-      .call(Connection.get(), sql)
+  query: (sql: string, params: any = [], logSQL: boolean = true): any =>
+    getQueryPromise()
+      .call(Connection.get(), sql, params)
       .then(value => {
-        Logger(DB_LOG, `${toLine(`${logSQL ? sql : '-'}; OK`)}`);
+        Logger(DB_LOG, `${toLine(`${logSQL ? `${sql} [${params}]` : '-'}; OK`)}`);
         return value;
       }, reason => {
-        const message = `${toLine(`${logSQL ? sql : '-'}; (${reason.message})`)}`;
+        const message = `${toLine(`${logSQL ? `${sql} [${params}]` : '-'}; (${reason.message})`)}`;
         Logger(DB_LOG, message);
         Logger(ERROR_LOG, message);
         handleConnectionError(reason, 'Query');
