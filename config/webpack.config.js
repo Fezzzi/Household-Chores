fa1a9eb8bb0b7a27bbe719dd0ba05d74bdbe1d46
Fezzzi <filip.horky.cz@gmail.com>
2020-08-08 02:17:04 +0200

    #30 - Fix main.js serving

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
   },
   entry: {
     js: ['babel-polyfill', 'clientSrc/index.jsx'],
