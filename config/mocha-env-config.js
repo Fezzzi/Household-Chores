c0fc22cf2f4e68198278ce9de4dd43d336bbc0a4
Fezzzi <filip.horky.cz@gmail.com>
2020-09-27 21:55:17 +0200

    #35 - Fix mocha test

--- config/mocha-env-config.js
+++ config/mocha-env-config.js
@@ -1,5 +1,7 @@
 const ignoreImports = () => null;
 
+process.env.MOCHA_TEST = true;
+
 require.extensions['.css'] = ignoreImports;
 require.extensions['.png'] = ignoreImports;
 require.extensions['.jpg'] = ignoreImports;
