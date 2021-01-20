37c7d51471a9de2e8f31cd16bae0e2ba0b5cc14a
Filip <filip.horky.cz@gmail.com>
2021-01-21 00:28:44 +0100

    #44 - Add confirmation dialogs table

---
+++ code/server/src/database/models/tables/dialogs.ts
@@ -0,0 +1,8 @@
+export default {
+  name: 'dialogs',
+  columns: {
+    id_user: 'id_user',
+    tutorial: 'tutorial',
+    household_user_deleting: 'state',
+  },
+}
--- config/migration-template.js
+++ config/migration-template.js
@@ -1,6 +1,6 @@
 import { migrateWithQueries } from 'serverSrc/helpers/migrations'
 
 module.exports = {
-  up: async (conn, cb) => migrateWithQueries(cb, true),
-  down: async (conn, cb) => migrateWithQueries(cb, true),
+  up: async (conn, cb) => await migrateWithQueries(cb, async () => {}),
+  down: async (conn, cb) => await migrateWithQueries(cb, async () => {}),
 }
