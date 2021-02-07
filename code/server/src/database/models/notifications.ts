6de869bee22e42240f196ea801b1883d030caa16
Filip <filip.horky.cz@gmail.com>
2021-02-07 21:00:00 +0100

    #64 - More work on API specification + associated refactoring

--- code/server/src/database/models/notifications.ts
+++ code/server/src/database/models/notifications.ts
@@ -3,7 +3,7 @@ import { NOTIFICATIONS } from 'shared/constants/mappingKeys'
 
 import { tNotifySettingsName, tNotifySettingsCols } from './tables'
 
-export const findNotificationSettings = async (userId: number): Promise<Record<string, any>> => {
+export const findNotificationSettings = async (userId: number): Promise<Record<string, Record<string, boolean>>> => {
   const result = await database.query(`
     SELECT ${Object.values(tNotifySettingsCols).filter(column => column !== tNotifySettingsCols.id_user).join(', ')}
     FROM ${tNotifySettingsName} WHERE ${tNotifySettingsCols.id_user}=${userId}
@@ -11,20 +11,20 @@ export const findNotificationSettings = async (userId: number): Promise<Record<s
   if (result[0]) {
     return {
       [NOTIFICATIONS.GENERAL]: {
-        email_notifications: result[0][tNotifySettingsCols.email_notifications],
-        browser_notifications: result[0][tNotifySettingsCols.browser_notifications],
-        mobile_notifications: result[0][tNotifySettingsCols.mobile_notifications],
+        [tNotifySettingsCols.email_notifications]: result[0][tNotifySettingsCols.email_notifications],
+        [tNotifySettingsCols.browser_notifications]: result[0][tNotifySettingsCols.browser_notifications],
+        [tNotifySettingsCols.mobile_notifications]: result[0][tNotifySettingsCols.mobile_notifications],
       },
       [NOTIFICATIONS.CONNECTIONS]: {
-        connection_approval: result[0][tNotifySettingsCols.connection_approval],
-        connection_request: result[0][tNotifySettingsCols.connection_request],
+        [tNotifySettingsCols.connection_approval]: result[0][tNotifySettingsCols.connection_approval],
+        [tNotifySettingsCols.connection_request]: result[0][tNotifySettingsCols.connection_request],
       },
       [NOTIFICATIONS.HOUSEHOLDS]: {
-        household_deleting: result[0][tNotifySettingsCols.household_deleting],
-        household_expelling: result[0][tNotifySettingsCols.household_expelling],
-        household_invitation: result[0][tNotifySettingsCols.household_invitation],
-        household_joining: result[0][tNotifySettingsCols.household_joining],
-        household_leaving: result[0][tNotifySettingsCols.household_leaving],
+        [tNotifySettingsCols.household_deleting]: result[0][tNotifySettingsCols.household_deleting],
+        [tNotifySettingsCols.household_expelling]: result[0][tNotifySettingsCols.household_expelling],
+        [tNotifySettingsCols.household_invitation]: result[0][tNotifySettingsCols.household_invitation],
+        [tNotifySettingsCols.household_joining]: result[0][tNotifySettingsCols.household_joining],
+        [tNotifySettingsCols.household_leaving]: result[0][tNotifySettingsCols.household_leaving],
       },
     }
   }
