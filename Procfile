ec2d396cae1cac1902f3c79207ee717d33578ab6
Filip <filip.horky.cz@gmail.com>
2021-09-06 22:47:04 +0200

    #77 - Fix db migration on Heroku release

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bash ./config/heroku/start-api.sh
-release: npm run migrate --reject-unauthorized false up
+release: bash ./config/heroku/release.sh
---
+++ config/heroku/release.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -eu
+
+echo "Run migrations"
+ts-node -r tsconfig-paths/register node_modules/node-pg-migrate/bin/node-pg-migrate -m code/server/migrations --reject-unauthorized false
