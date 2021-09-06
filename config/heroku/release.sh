cc3eb7d7f4fdd2a23977f1a8415be814234830fa
Filip <filip.horky.cz@gmail.com>
2021-09-06 22:56:40 +0200

    #77 - Fix db migration on Heroku release

--- config/heroku/release.sh
+++ config/heroku/release.sh
@@ -3,4 +3,4 @@
 set -eu
 
 echo "Run migrations"
-ts-node -r tsconfig-paths/register node_modules/node-pg-migrate/bin/node-pg-migrate -m code/server/migrations --reject-unauthorized false
+ts-node -r tsconfig-paths/register node_modules/node-pg-migrate/bin/node-pg-migrate -m code/server/migrations --reject-unauthorized false up
